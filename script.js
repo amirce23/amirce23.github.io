836006cdd970616339e419c891d0b7993a129f70
GGmax11 <amyrbhdad23@gmail.com>
2025-04-29 18:47:11 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+  AOS.init({
+    duration: 1000, // مدت زمان انیمیشن بر حسب میلی‌ثانیه
+    once: true      // انیمیشن فقط یک بار نمایش داده شود
+  });
+});
