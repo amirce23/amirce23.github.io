46a30b0b7be5c83e3b741dcb14a650b3d5180e7e
GGmax11 <amyrbhdad23@gmail.com>
2025-09-10 02:42:46 -0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// assets/js/script.js
-document.addEventListener('DOMContentLoaded', () => {
-  // فعال‌سازی AOS
-  AOS.init({ duration:1000, once:true });
-
-  // منوی موبایل (در صورت نیاز)
-  const logo = document.querySelector('.logo');
-  // می‌توانید اینجا کد منوی همبرگری اضافه کنید
-});
